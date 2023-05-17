d16b4f45632c4a95367908370095d9905d9875bd
VectorK2 <balov.vad97@mail.ru>
2023-05-17 03:38:16 +0300

    commit

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const popup = document.querySelector('#popup');
 const profileEdit = document.querySelector('#profile_edit');
+const cross = document.querySelector('#popup_close');
+const name = document.querySelector('#name');
+const status = document.querySelector('#status');
+const inputName = document.querySelector('#name_input');
+const inputStatus = document.querySelector('#status_input');
+const popupButton = document.querySelector('#popup_button');
+const likeChange1 = document.querySelector('#elements_like-1');
+const likeChange2 = document.querySelector('#elements_like-2');
+const likeChange3 = document.querySelector('#elements_like-3');
+const likeChange4 = document.querySelector('#elements_like-4');
+const likeChange5 = document.querySelector('#elements_like-5');
+const likeChange6 = document.querySelector('#elements_like-6');
+
 
 function clickProfileEdit() {
     popup.classList.add('popup_active');
@@ -7,23 +20,15 @@ function clickProfileEdit() {
 
 profileEdit.addEventListener('click', clickProfileEdit);
 
-const cross = document.querySelector('#popup_close');
-
 function popupClick() {
     popup.classList.remove('popup_active');
 }
 
 cross.addEventListener('click', popupClick);
 
-const name = document.querySelector('#name');
-const status = document.querySelector('#status');
-
-const inputName = document.querySelector('#name_input');
 inputName.value = name.textContent;
-const inputStatus = document.querySelector('#status_input');
 inputStatus.value = status.textContent;
 
-const popupButton = document.querySelector('#popup_button');
 
 function clickPopupButton() {
     name.textContent = inputName.value;
@@ -33,4 +38,38 @@ function clickPopupButton() {
 
 popupButton.addEventListener('click', clickPopupButton);
 
+likeChange1.addEventListener('click', likeBlackChange1);
 
+function likeBlackChange1() {
+    likeChange1.src = 'images/likeblack.jpg'
+}
+
+likeChange2.addEventListener('click', likeBlackChange2);
+
+function likeBlackChange2() {
+    likeChange2.src = 'images/likeblack.jpg'
+}
+
+likeChange3.addEventListener('click', likeBlackChange3);
+
+function likeBlackChange3() {
+    likeChange3.src = 'images/likeblack.jpg'
+}
+
+likeChange4.addEventListener('click', likeBlackChange4);
+
+function likeBlackChange4() {
+    likeChange4.src = 'images/likeblack.jpg'
+}
+
+likeChange5.addEventListener('click', likeBlackChange5);
+
+function likeBlackChange5() {
+    likeChange5.src = 'images/likeblack.jpg'
+}
+
+likeChange6.addEventListener('click', likeBlackChange6);
+
+function likeBlackChange6() {
+    likeChange6.src = 'images/likeblack.jpg'
+}
