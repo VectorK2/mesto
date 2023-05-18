446a4be25abb44ba5cff2d4bad5bc82f0bf8fbbf
VectorK2 <balov.vad97@mail.ru>
2023-05-18 20:16:15 +0300

    commit

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+const popup = document.querySelector('#popup');
+const profileEdit = document.querySelector('#profile_edit');
+const cross = document.querySelector('#popup_close');
+const name = document.querySelector('#name');
+const status = document.querySelector('#status');
+const inputName = document.querySelector('#name_input');
+const inputStatus = document.querySelector('#status_input');
+
+
+function clickProfileEdit() {
+    popup.classList.add('popup_active');
+    inputName.value = name.textContent;
+    inputStatus.value = status.textContent;
+}
+
+
+profileEdit.addEventListener('click', clickProfileEdit);
+
+function popupClick() {
+    popup.classList.remove('popup_active');
+    inputName.value = name.textContent;
+    inputStatus.value = status.textContent;
+}
+
+cross.addEventListener('click', popupClick);
+
+
+function clickPopupButton() {
+
+    name.textContent = inputName.value;
+    status.textContent = inputStatus.value;
+    popupClick();
+}
+
+popupButton.addEventListener('click', clickPopupButton);
+
+likeChange1.addEventListener('click', likeBlackChange1);
